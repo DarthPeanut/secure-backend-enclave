0b57ebede048f44ae81b1dcccfb04d9cbc4ee266
Geo Mitchell <geo.mitchell@yahoo.com>
2026-05-29 21:51:32 -0500

    chore: fixed a provider issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_container_registry" "acr" {
     admin_enable = true
 }
 #Free Tier
-resource "azure_log_analytics_workspace" "law"{
+resource "azurerm_log_analytics_workspace" "law"{
     name = "platform-logs"
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
